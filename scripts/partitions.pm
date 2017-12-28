cdcb3ddc8f14c42731ce5ce55cad3ae859ed6427
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-12-29 00:53:42 +0600

    build-system: add stub for is_disk_partitioned()
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ scripts/partitions.pm
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+#
+# The build-system is Copyright (C) 2017 Alexander Kuleshov <kuleshovmail@gmail.com>,
+#
+# Github: https://github.com/0xAX/kernel-dev/tree/master/kernel-testing
+
+use strict;
+use warnings "all";
+use diagnostics;
+
+sub is_disk_partitioned() {
+    return 1;
+}
+
+1;
