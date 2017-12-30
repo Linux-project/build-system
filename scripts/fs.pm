1d2ec589a273f66e9d91724eb24cde708aa28676
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-12-30 19:26:47 +0600

    fs: get partitions offsets in make_fs()
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/fs.pm
+++ scripts/fs.pm
@@ -4,4 +4,14 @@
 #
 # Github: https://github.com/0xAX/kernel-dev/tree/master/kernel-testing
 
+use Data::Dumper;
+
+sub make_fs {
+    $config = shift;
+    $image = shift;
+
+    my $disk_info = `sfdisk -d $image`;
+    my @partitions = ($disk_info =~ /start\=\s+([0-9]*),/g);
+}
+
 1;
