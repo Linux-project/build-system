b9d185b34ac46216131201d300371b6ac3e20e0f
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-12-30 19:10:06 +0600

    fs: add initial script
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ scripts/fs.pm
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+#
+# The build-system is Copyright (C) 2017 Alexander Kuleshov <kuleshovmail@gmail.com>,
+#
+# Github: https://github.com/0xAX/kernel-dev/tree/master/kernel-testing
+
+1;
