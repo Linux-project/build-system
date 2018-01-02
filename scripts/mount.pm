7eb4c26e30694a5662feda7bdd9fd49e9baf4160
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-01-02 18:10:19 +0600

    mount: add initial mount script
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ scripts/mount.pm
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+#
+# The build-system is Copyright (C) 2017 Alexander Kuleshov <kuleshovmail@gmail.com>,
+#
+# Github: https://github.com/0xAX/kernel-dev/tree/master/kernel-testing
