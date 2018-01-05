1f90f9931ef138a6e0dd5ffad431c768fbd6b881
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-01-05 19:45:37 +0600

    initramfs: initial handling of local initrd
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/provision/initramfs.pm
+++ scripts/provision/initramfs.pm
@@ -3,4 +3,48 @@
 #
 # Github: https://github.com/0xAX/kernel-dev/tree/master/kernel-testing
 
+use Data::Dumper;
+
+sub process_local_initrd {
+    my $root = shift;
+    my $initrd = shift;
+    my $initrd_path = '';
+
+    if (!defined($initrd->{path})) {
+        $initrd_path = "./initrds/x86_64/initrd_x86_64.cpio.gz"    
+    } else {
+        $initrd_path = $initrd->{path};
+    }
+
+    # put initramfs image to /boot directory
+}
+
+sub process_docker_initrd {
+    my $root = shift;
+}
+
+sub provision_initramfs {
+    my $config = shift;
+    my $root = shift;
+    my $type = '';
+
+    die "[error] mandatory field in specification is missed - initrd."
+        unless defined($config->{initrd});
+
+    my $initrd = $config->{initrd};
+
+    if (!defined($initrd->{type})) {
+        $type = 'local';
+    } else {
+        $type = $initrd->{type};
+    }
+
+    if ($type eq 'local') {
+        return process_local_initrd($root, $initrd);
+    }
+    elsif ($type eq 'docker') {
+        return process_docker_initrd($root, $initrd);        
+    }
+}
+
 1;
