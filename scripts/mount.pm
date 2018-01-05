e6227bb3bacefcb39e4e35dda4e4554bc7dc3695
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-01-05 15:15:20 +0600

    mount: mount all partitions noted in specification
    Additionally this patch provides a little bug fix for umounting of
    partitions.
    
    We should umount `/` in the end, because of busyness.
    
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/mount.pm
+++ scripts/mount.pm
@@ -7,20 +7,27 @@
 use Data::Dumper;
 
 sub umount_partitions {
-    my $root_dir = basename(shift);
+    my $partitions = shift;
+    my %partitions = %{$partitions};
 
-    my $mounted_fs = `mount | grep $root_dir`;
-
-    if ($mounted_fs ne "") {
-        my @mounted_fs = split /\n/, $mounted_fs;
-        foreach (@mounted_fs) {
-            $_ =~ s/\s.*//;
-            my $ret = system("umount", $_);
-
-            if ($ret != 0) {
-                print STDERR "[error] something going wrong during umount of $_";
-            }
+    while (($k, $v) = each (%partitions)) {
+        if ($k eq "/") {
+            next;
         }
+
+        print "[info] umount $k binded to $v\n";
+        my $ret = system("umount", $v);
+        if ($ret != 0) {
+            print STDERR "[error] something going wrong during umount of $k binded to $v\n";
+        }
+    }
+
+    my $root_device= $partitions{"/"};
+
+    print "[info] umount / binded to $root_device\n";
+    my $ret = system("umount", $root_device);
+    if ($ret != 0) {
+        print STDERR "[error] something going wrong during umount of / binded to $root_device\n";
     }
 }
 
@@ -62,8 +69,23 @@ sub mount_partitions {
 
     # mount other directories
     foreach (@{$config->{partitions}}) {
+        my $fs_type = $_->{fs}[0]->{name};
+        my $device = $partitions{$_->{mountPoint}};
+
         if ($_->{mountPoint} eq '/') {
             next;
+        }
+
+        my $mount_point = $root_dir . $_->{mountPoint};
+        mkdir($mount_point);
+
+        print "[info] mount $_->{mountPoint} partition to $device with $fs_type fs\n";
+        my $ret = `mount -t $fs_type $device $mount_point >/dev/null 2>&1`;
+
+        if ($? != 0) {
+            print STDERR "[error] mount of $device to $mount_point failed\n";
+            print STDERR "Try to execute mount -t $fs_type $device $mount_point manually\n";
+            return -1;
         }
     }
 
